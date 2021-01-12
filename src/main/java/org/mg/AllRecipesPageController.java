19abcc28a23743dd167a22b23f403388fd76cbfc
Unknown807 <milvovangveric@gmail.com>
2021-01-12 15:04:59 +0000

    Can remove recipes from the tableview

--- src/main/java/org/mg/AllRecipesPageController.java
+++ src/main/java/org/mg/AllRecipesPageController.java
@@ -5,16 +5,13 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -38,6 +35,55 @@ public class AllRecipesPageController implements Initializable {
     @FXML
     private void switchToCreateRecipePage() throws IOException {
         App.setRoot("CreateRecipePage");
+    }
+
+    @FXML
+    private void viewRecipe() {
+
+    }
+
+    @FXML
+    private void removeRecipes() {
+        ObservableList<Recipe> recipesToRemove = recipeTable.getSelectionModel().getSelectedItems();
+        if (recipesToRemove.size() < 1)
+            return;
+
+        Alert confirmRemoval = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmRemoval.setTitle("Confirm");
+        confirmRemoval.setHeaderText("Recipe Removal");
+        confirmRemoval.setContentText("Are you sure you want to remove the selected recipes?");
+
+        Optional<ButtonType> result = confirmRemoval.showAndWait();
+        if (result.get() == ButtonType.OK) {
+
+            ObservableList<Recipe> newItems = recipeTable.getItems();
+            Boolean success = true;
+
+            for (Recipe recipe: recipesToRemove) {
+                File file = new File("./"+recipe.getRecipeName()+".json");
+                if (!(file.delete())) {
+                    Alert failure = new Alert(Alert.AlertType.ERROR);
+                    failure.setTitle("File Deletion Error");
+                    failure.setHeaderText(null);
+                    failure.setContentText("The file "+recipe.getRecipeName()+" could not be deleted");
+                    failure.showAndWait();
+                    success = false;
+                } else {
+                    newItems.remove(recipe);
+                }
+            }
+
+            recipeTable.getSelectionModel().clearSelection();
+
+            if (success) {
+                Alert finish = new Alert(Alert.AlertType.INFORMATION);
+                finish.setTitle("Recipes Removed");
+                finish.setHeaderText(null);
+                finish.setContentText("The recipes have been removed");
+                finish.showAndWait();
+            }
+        }
+
     }
 
     @Override
